5835dc1cc73171ff93efdff57625bb1dca3c5775
rupesh8690 <rupeshlove8690@gmail.com>
2025-10-02 16:19:20 +0530

    fixed the issue in send email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,32 +1,32 @@
-require('dotenv').config();
+require("dotenv").config();
 const nodemailer = require("nodemailer");
 
-
 async function sendEmail({ email, subject, message }) {
-    try {
-        const transporter = nodemailer.createTransport({
-            service: "smtp.gmail.com",
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS
-            }
-        });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // use TLS
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
-            to: email,
-            subject,
-            text: message
-        };
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject,
+      text: message,
+    };
 
-        const info = await transporter.sendMail(mailOptions);
-        console.log("Email sent: " + info.response);
-        return info;
-
-    } catch (error) {
-        console.error("Error sending email:", error);
-        throw new Error("Email could not be sent");
-    }
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: " + info.response);
+    return info;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw new Error("Email could not be sent");
+  }
 }
 
 module.exports = sendEmail;
